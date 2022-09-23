ece1572b2d89b49f39d18cb76ef54cddda22daaa
Matthew Ko <iammatthewko114@gmail.com>
2022-09-24 00:21:50 +0800

    Initial commit

---
+++ hardhat-tutorial/.history/contracts/Whitelist_20220831182612.sol
@@ -0,0 +1,73 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+
+contract Whitelist {
+
+    // Max number of whitelisted addresses allowed
+    uint8 public maxWhitelistedAddresses;
+
+    // Create a mapping of whitelistedAddresses
+    // if an address is whitelisted, we would set it to true, it is false by default for all other addresses.
+    mapping(address => bool) public whitelistedAddresses;
+
+    // numAddressesWhitelisted would be used to keep track of how many addresses have been whitelisted
+    // NOTE: Don't change this variable name, as it will be part of verification
+    uint8 public numAddressesWhitelisted;
+
+    // Setting the Max number of whitelisted addresses
+    // User will put the value at the time of deployment
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses =  _maxWhitelistedAddresses;
+    }
+
+    /**
+        addAddressToWhitelist - This function adds the address of the sender to the
+        whitelist
+     */
+    function addAddressToWhitelist() public {
+        // check if the user has already been whitelisted
+        require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
+        // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
+        require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
+        // Add the address which called the function to the whitelistedAddress array
+        whitelistedAddresses[msg.sender] = true;
+        // Increase the number of whitelisted addresses
+        numAddressesWhitelisted += 1;
+    }
+
+}
+Lets deploy the contract to rinkeby network. Create a new file, or replace the default file named deploy.js under the scripts folder
+
+Now we will write some code to deploy the contract in deploy.js file.
+
+const { ethers } = require("hardhat");
+
+async function main() {
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so whitelistContract here is a factory for instances of our Whitelist contract.
+  */
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
+
+  // here we deploy the contract
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+  // 10 is the Maximum number of whitelisted addresses allowed
+  
+  // Wait for it to finish deploying
+  await deployedWhitelistContract.deployed();
+
+  // print the address of the deployed contract
+  console.log(
+    "Whitelist Contract Address:",
+    deployedWhitelistContract.address
+  );
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
